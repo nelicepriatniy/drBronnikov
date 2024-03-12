538fe4be4932a105f1aa89f403a81cd78cd3c765
nelicepriatniy <92029297+nelicepriatniy@users.noreply.github.com>
2024-03-13 02:28:50 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -5,6 +5,21 @@ const plussSlider = new Swiper('.plussSLider', {
 });
 
 
+const closePopups = document.querySelector('.closePopups'),
+      popups = document.querySelectorAll('.popup');
+
+if(closePopups) {
+    closePopups.onclick = ()=>{
+        closePopups.classList.remove('active')
+        if(popups.length > 0) {
+            for(let i = 0; popups.length > i; i++) {
+                popups[i].classList.remove('active')
+            }
+        }
+    }
+}
+
+
 let staffItems = document.querySelectorAll('.staffItem');
 
 if(staffItems.length > 0) {
@@ -15,12 +30,14 @@ if(staffItems.length > 0) {
       for(let o = 0; staffItems.length > o; o++) {
           staffItems[o].classList.remove('active')
       }
+      closePopups.classList.add('active')
       staffItems[i].classList.add('active')
     }
     closeBtn.onclick = ()=> {
       console.log(123)
       for(let o = 0; staffItems.length > o; o++) {
           staffItems[o].classList.remove('active')
+          closePopups.classList.remove('active')
       }
     }
   }
@@ -36,3 +53,69 @@ if(dealsItems.length > 0) {
     }
   }
 }
+
+
+window.onscroll = () => {
+  if (window.scrollY > 200) {
+      document.querySelector('header').classList.add('dixedActive')
+  }
+  if (window.scrollY > 100) {
+      document.querySelector('header').classList.add('dixed')
+  } else {
+      document.querySelector('header').classList.remove('dixed')
+      document.querySelector('header').classList.remove('dixedActive')
+  }
+}
+
+
+
+let mobbtn = document.querySelector('.headerMobMenuOpen');
+let menu = document.querySelector('.headerMobMenu');
+let headerMenuClose = document.querySelector('.headerMobMenuCloseBtn')
+
+mobbtn.addEventListener('click', function () {
+    mobbtn.classList.toggle('active')
+    menu.classList.toggle('active');
+})
+
+headerMenuClose.onclick = ()=>{
+  mobbtn.classList.remove('active')
+  menu.classList.remove('active');
+}
+
+
+let mobbtnb = document.querySelectorAll('.lineb');
+mobbtnb.forEach(element => {
+    element.addEventListener('click', function () {
+        mobbtn.classList.toggle('active')
+        menu.classList.toggle('active');
+    })
+});
+
+
+
+function offsetPosition(element) {
+  var offsetLeft = 0, offsetTop = 0;
+  do {
+      offsetLeft += element.offsetLeft;
+      offsetTop += element.offsetTop;
+  } while (element = element.offsetParent);
+  return offsetTop;
+}
+
+let scrollBtn = document.querySelectorAll('.scrollbtn');  //класс кнопок для скролла
+scrollBtn.forEach(el => {
+  let elem = el;
+  el.addEventListener('click', function () {
+    menu.classList.remove('active')
+      let data = elem.getAttribute('data-b'); 
+      let block = document.querySelector(data);
+      let offset = offsetPosition(block);
+      window.scrollTo({
+          top: offset,
+          behavior: 'smooth',
+          
+      });
+      console.log(offset)
+  })
+});
